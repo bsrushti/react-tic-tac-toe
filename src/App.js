bbae6b1420399857e068f36c7c7bf91c31693886
bsrushti <bsrushti@in-bsrushti.local>
2019-04-01 09:15:26 +0530

    Created tic-tac-toe game board.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-class App extends Component {
+import "./App.css";
+
+class Square extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <li
+        className={this.props.className}
+        id={this.props.id}
+        style={{ width: 70 }}
+        onClick={this.props.onClick}
+      >
+        {this.props.value}
+      </li>
     );
   }
 }
 
-export default App;
+class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { content: "#" };
+  }
+
+  tic(e) {
+    this.setState({ content: "*" });
+    console.log(e.target.txt);
+    return e.target;
+  }
+
+  renderCell(id, value) {
+    return (
+      <Square
+        value={value}
+        className="tic"
+        id={id}
+        onClick={this.tic.bind(this)}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <ul id="gameBoard" style={{ width: 350 }}>
+          {this.renderCell(0, this.state.content)}
+          {this.renderCell(1, this.state.content)}
+          {this.renderCell(2, this.state.content)}
+          {this.renderCell(3, this.state.content)}
+          {this.renderCell(4, this.state.content)}
+          {this.renderCell(5, this.state.content)}
+          {this.renderCell(6, this.state.content)}
+          {this.renderCell(7, this.state.content)}
+          {this.renderCell(8, this.state.content)}
+        </ul>
+      </div>
+    );
+  }
+}
+export default Game;
