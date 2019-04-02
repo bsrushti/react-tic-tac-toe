14afdeb3cd675d96b926bf4fbe231c679656af9c
bsrushti <bsrushti@in-bsrushti.Dlink>
2019-04-02 18:05:57 +0530

    Updates

---
+++ precache-manifest.c354377346716915bbde72f72338fbda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f6ad56159a8cf147f55",
+    "url": "/react-tic-tac-toe/static/css/main.0fa41ace.chunk.css"
+  },
+  {
+    "revision": "1f6ad56159a8cf147f55",
+    "url": "/react-tic-tac-toe/static/js/main.9346b19b.chunk.js"
+  },
+  {
+    "revision": "140d55aca7c0751087c1",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.a5f725ed.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-tic-tac-toe/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "fa7929a8d9a4a3e9c63b29093643ebed",
+    "url": "/react-tic-tac-toe/index.html"
+  }
+];
